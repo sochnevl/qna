33f1f361aaf25e337fba01fa2d8f5f6fc6c32a69
Alex <alexiamxsmart@gmail.com>
2024-05-09 00:30:57 +0300

    add(some settings): работа с вью, настройки генератора, роуты, фабрики

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,8 @@ module Qna
                        view_specs: false,
                        helper_specs: false,
                        routing_specs: false,
-                       request_specs: false
+                       request_specs: false,
+                       controller_specs: true
     end
   end
 end
